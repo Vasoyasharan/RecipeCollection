1f5020d43fb1ed64d76c56467cdb137447e05282
vasoyasharan <sharanvasoya@gmail.com>
2024-07-21 20:22:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Display current year in the footer
+const currentYear = new Date().getFullYear();
+document.querySelector('#current-year').textContent = currentYear;
